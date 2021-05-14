a4383ba269f8efff93e2b06716f03fa55f777394
Ben Stephens Hemingway <27730685+bsh2@users.noreply.github.com>
2021-05-14 19:34:29 +0100

    Added new files

--- software/utilities/standard_ffm_objective.R
+++ software/utilities/standard_ffm_objective.R
@@ -1,19 +1,7 @@
 # ------------------------------------------------------------------------------
-# Example inputs for functions contained in the file
-# ------------------------------------------------------------------------------
-
-# pars <- c(100, 1, 25, 1.2, 5)  # Parameters in order (p*, k_g, Tau_g, k_h, Tau_h)
-
-# Training loads (inputs)
-# loads <- data.frame("day" = c(0,1,2,3,4,5), "load" = c(100, 100, 100, 100, 100))
-
-# Measured performances (model target)
-# perfVals <- data.frame("day" = c(3,4), "performance" = c(500, 200))
-
-# ------------------------------------------------------------------------------
 # Objective function (Residual sum of squares) - Standard FFM
 # ------------------------------------------------------------------------------
-standardObjective <- function(pars, loads, perfVals){
+standardObjectiveLS <- function(pars, loads, perfVals){
   
   nMeasurements <- length(perfVals$performance) # Number of performance measurements
   
