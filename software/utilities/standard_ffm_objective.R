8b89f9fc4da7480985a6d11ee736482e1f49e7e7
Ben Stephens Hemingway <27730685+bsh2@users.noreply.github.com>
2021-05-14 20:06:15 +0100

    standard model objective fn

--- software/utilities/standard_ffm_objective.R
+++ software/utilities/standard_ffm_objective.R
@@ -1,7 +1,15 @@
 # ------------------------------------------------------------------------------
 # Objective function (Residual sum of squares) - Standard FFM
 # ------------------------------------------------------------------------------
-standardObjectiveSS <- function(pars, loads, perfVals, maximise = FALSE){
+standardObjectiveSS <- function(pars, loads, perfVals, initial = FALSE,
+                                maximise = FALSE){
+  
+  # INPUT NOTES:
+  # ----------------------------------------------------
+  #         [1] [2] [3] [4] [5] [6] [7]
+  # Pars: c(p*, kg, Tg, kh, Th)          initial = FALSE
+  # Pars: c(p*, kg, Tg, kh, Th, qg, qh)  initial = TRUE
+  # ----------------------------------------------------
   
   nMeasurements <- length(perfVals$performance) # Number of performance measurements
   
@@ -18,8 +26,16 @@ standardObjectiveSS <- function(pars, loads, perfVals, maximise = FALSE){
     # Note: 1:dayT rather than 1:(dayT - 1) as the first row in the loads array is w(0)=0
     inputSubset <- loads[1:dayT, ]
     
+    if (initial == TRUE){
+      initFitness <- pars[6] * exp(-(1:dayT) / pars[3])
+      initFatigue <- pars[7] * exp(-(1:dayT) / pars[5])
+    } else{
+      initFitness <- 0
+      initFatigue <- 0
+    }
+    
     # Compute modelled performance on dayT under pars // p\hat(dayT) = p* + g(dayT) - h(dayT)
-    model <- pars[1] + 
+    model <- pars[1] + initFitness - initFatigue +
       pars[2] * (sum(inputSubset$load * exp(-(dayT - inputSubset$day) / pars[3]) ) ) -
       pars[4] * (sum(inputSubset$load * exp(-(dayT - inputSubset$day) / pars[5]) ) )
     
@@ -37,5 +53,59 @@ standardObjectiveSS <- function(pars, loads, perfVals, maximise = FALSE){
   }
 }
 
+# ------------------------------------------------------------------------------
+# Objective function (Log likelihood) - Standard FFM
+# ------------------------------------------------------------------------------
 
-
+standardObjectiveLL <- function(pars, loads, perfVals, initial = FALSE, 
+                              maximise = FALSE){
+  
+  # INPUT NOTES:
+  # ---------------------------------------------------------------------------
+  #         [1] [2] [3] [4] [5] [6]    [7] [8]
+  # Pars: c(p*, kg, Tg, kh, Th, sigma)                  initial = FALSE
+  # Pars: c(p*, kg, Tg, kh, Th, sigma, qg, qh)          initial = TRUE
+  # ---------------------------------------------------------------------------
+  
+  convolveTraining <- function(loads, tau){
+    
+    # Value of t relevant to (eq 6.9) 
+    dayt <- length(loads)
+    
+    # Note that loads[1:dayt] will yield c(w(0), w(1), ... , w(t-1))
+    return(sum(loads[1:dayt] * exp(-(dayt:1 / tau))))
+  }
+  
+  finalMeasurement <- tail(perfVals$day, 1)
+  
+  if (initial == TRUE){
+    initFitness <- pars[7] * exp(-(1:finalMeasurement) / pars[3])
+    initFatigue <- pars[8] * exp(-(1:finalMeasurement) / pars[5])
+  }
+  
+  # Compute modeled performance from t=1 to t=finalMeasurement
+  modFitness <- pars[2] * sapply(1:finalMeasurement, function(t) convolveTraining(loads$load[1:t], pars[3]))
+  modFatigue <- pars[4] * sapply(1:finalMeasurement, function(t) convolveTraining(loads$load[1:t], pars[5]))
+  
+  if (initial == FALSE){
+    modPerformance <- pars[1] + modFitness - modFatigue
+  }
+  
+  if (initial == TRUE){
+    modPerformance <- pars[1] + initFitness - initFatigue + modFitness - modFatigue
+  }
+  
+  # Extract modeled performance values on days where measurement exists
+  modPerformance <- modPerformance[perfVals$day]
+  
+  # Compute errors
+  errors <- perfVals$performance - modPerformance
+  
+  if (maximise = FALSE){
+    return(-1.0 * sum(dnorm(errors, mean = 0, sd = pars[6], log = TRUE)))
+  }
+  if (maximise = TRUE){
+    return(sum(dnorm(errors, mean = 0, sd = pars[6], log = TRUE)))
+  }
+  
+}
